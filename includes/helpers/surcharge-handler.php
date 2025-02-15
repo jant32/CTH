1a284b4f4ec19d7b4aebf7f68ff4e723a39154a0
jant32 <105398272+jant32@users.noreply.github.com>
2025-02-15 11:45:59 +0100

    Korrektur falsche Zuschlagsberechnung

--- includes/helpers/surcharge-handler.php
+++ includes/helpers/surcharge-handler.php
@@ -8,6 +8,7 @@ add_action('woocommerce_cart_calculate_fees', function() {
     $customer_type = WC()->session->get('customer_type', 'verein_ssb');
     $cart = WC()->cart;
     $surcharge_percentage = 0;
+    $surcharge_name = 'Kundenart-Zuschlag';
 
     switch ($customer_type) {
         case 'verein_non_ssb':
@@ -21,16 +22,27 @@ add_action('woocommerce_cart_calculate_fees', function() {
             break;
     }
 
-    // Vorhandene Zuschläge entfernen, um doppelte Einträge zu vermeiden
-    foreach ($cart->get_fees() as $fee_key => $fee) {
-        if ($fee->name === 'Kundenart-Zuschlag') {
-            $cart->remove_fee($fee_key);
+    // Überprüfung, ob der Zuschlag bereits existiert
+    $existing_surcharge = false;
+    foreach ($cart->get_fees() as $fee) {
+        if ($fee->name === $surcharge_name) {
+            $existing_surcharge = true;
+            break;
+        }
+    }
+
+    // Falls der Zuschlag bereits existiert, entfernen
+    if ($existing_surcharge) {
+        foreach ($cart->get_fees() as $fee_key => $fee) {
+            if ($fee->name === $surcharge_name) {
+                $cart->remove_fee($fee_key);
+            }
         }
     }
 
     // Neuen Zuschlag berechnen und hinzufügen
     if ($surcharge_percentage > 0) {
         $surcharge_amount = $cart->cart_contents_total * $surcharge_percentage;
-        $cart->add_fee('Kundenart-Zuschlag', $surcharge_amount, true);
+        $cart->add_fee($surcharge_name, $surcharge_amount, true);
     }
-}, 10, 1);
+}, 20, 1);
